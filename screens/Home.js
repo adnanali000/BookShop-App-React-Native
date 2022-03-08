8c5294efdf7d0cd4d49c50a40e72d1c8fa396a33
adnanali000 <adnan.museel123@gmail.com>
2022-03-08 23:04:40 +0500

    header buttons done

--- screens/Home.js
+++ screens/Home.js
@@ -9,6 +9,16 @@ import {
 
 import {SIZES,COLORS,FONTS,icons,images} from '../constants'
 
+
+const LineDivider = ()=>{
+    return(
+        <View style={{width:1,paddingVertical:18}}>
+            <View style={{flex:1,borderLeftColor:COLORS.lightGray,borderLeftWidth:1}}></View>
+        </View>
+    )
+}
+
+
 const Home = ()=>{
 
     const profileData = {
@@ -66,11 +76,94 @@ const Home = ()=>{
         )
     }
 
+    function renderButtonSection(){
+        return(
+            <View style={{
+                flex:1,
+                justifyContent:'center',
+                padding:SIZES.padding
+            }}>
+                <View style={{
+                    flexDirection:'row',
+                    backgroundColor:COLORS.secondary,
+                    height:70,
+                    borderRadius:SIZES.radius
+                }}>
+                    {/* claim  */}
+
+                    <TouchableOpacity style={{flex:1}} onPress={()=>alert('claim')}>
+                        <View style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+                            <Image 
+                                source={icons.claim_icon}
+                                resizeMode='contain'
+                                style={{
+                                    height:30,
+                                    width:30
+                                }}
+                            />
+                            <Text style={{...FONTS.body3,color:COLORS.white,marginLeft:SIZES.base}}>Claim</Text>
+
+                        </View>
+                    </TouchableOpacity>
+
+                    {/* divider  */}
+                    <LineDivider />
+
+                    {/* get point  */}
+                    <TouchableOpacity style={{flex:1}} onPress={()=>alert('point')}>
+                        <View style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+                            <Image 
+                                source={icons.point_icon}
+                                resizeMode='contain'
+                                style={{
+                                    height:30,
+                                    width:30
+                                }}
+                            />
+                            <Text style={{...FONTS.body3,color:COLORS.white,marginLeft:SIZES.base}}>Get Point</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    {/* divider  */}
+                    
+                    <LineDivider />
+
+                    {/* card  */}
+
+                    <TouchableOpacity style={{flex:1}} onPress={()=>alert('Card')}>
+                        <View style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+                            <Image 
+                                source={icons.card_icon}
+                                resizeMode='contain'
+                                style={{
+                                    height:30,
+                                    width:30
+                                }}
+                            />
+                            <Text style={{...FONTS.body3,color:COLORS.white,marginLeft:SIZES.base}}>Card</Text>
+                        </View>
+                    </TouchableOpacity>
+
+
+                </View>
+
+            </View>
+        )
+    }
+
     return(
         <SafeAreaView style={{flex:1,backgroundColor:COLORS.black}}>
+          
+          {/* header section  */}
+
             <View style={{height:200}}>
                 {renderHeader(profile)}
+                
+                {renderButtonSection()}  
             </View>
+
+            {/* body section  */}
+            
         </SafeAreaView>
     )
 }
